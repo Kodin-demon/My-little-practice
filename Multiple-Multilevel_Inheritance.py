7b4ad82b3f70da28f033e907fde4816c24cced27
Konstantin Nikonorov <153679544+Kodin-demon@users.noreply.github.com>
2025-08-22 17:17:32 +0200

    Add files via upload
    I can already sense the level of chaos that will happen while using this

---
+++ Multiple-Multilevel_Inheritance.py
@@ -0,0 +1,45 @@
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating")
+
+    def drink(self):
+        print(f"{self.name} is drinking")
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Prey(Animal):
+    def flee(self):
+        print(f"{self.name} is fleeing")
+
+class Predator(Animal):
+    def hunt(self):
+        print(f"{self.name} is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit("Bucks")
+hawk = Hawk("Tony")
+fish = Fish("Dori")
+
+rabbit.sleep()
+hawk.hunt()
+rabbit.flee()
+hawk.eat()
+hawk.drink()
+
+fish.flee()
+fish.hunt()
+
